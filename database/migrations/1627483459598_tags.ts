e8cb8af5cc9d3c031cb4a7a104ce78f0fda4155a
jamesyoung-info <me@jamesyoung.info>
2021-07-30 15:46:37 +1000

    Add tags and posts routes, unique key on tag name

--- database/migrations/1627483459598_tags.ts
+++ database/migrations/1627483459598_tags.ts
@@ -6,7 +6,7 @@ export default class Tags extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name', 255).notNullable()
+      table.string('name', 255).notNullable().unique()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
--- start/routes.ts
+++ start/routes.ts
@@ -23,8 +23,33 @@ import Application from '@ioc:Adonis/Core/Application'
 import * as dotenv from 'dotenv'
 import Hash from '@ioc:Adonis/Core/Hash'
 import User from 'App/Models/User'
+import Tag from 'App/Models/Tag'
+import Paste from 'App/Models/Paste'
 
 Route.group(() => {
+
+  // routes for tags and posts
+  Route.post('/tags', async ( {auth, request, response }) => {
+    // new tag
+    await auth.use('api').authenticate()
+    const tag = new Tag()
+    tag.name = request.input('tag')
+    await tag.save()
+    return response.created()
+  })
+
+  Route.get('/tags', async({ auth, request, response }) => {
+    // no auth necessary?
+    // do it anyway to prevent unauthenticated lists using bandwidth from server
+    await auth.use('api').authenticate()
+    const tags = await Tag.all()
+    return { tags: tags }
+  })
+
+  Route.post('/posts', async ({auth, request, response }) => {
+    await auth.use('api').authenticate()
+
+  })
 
   Route.post('/register', async ({ request, response }) => {
     const user = new User()
